c55a9ba719c76953a67b2c2ade8a124e1df02e43
Scott Milliorn <scottmilliorn@gmail.com>
2024-10-07 14:13:07 -0700

    Add files via upload
    Signed-off-by: Scott Milliorn <scottmilliorn@gmail.com>

---
+++ learning-playgrounds/This-Week-In-Rust/multi-threading/README.md
@@ -0,0 +1,3 @@
+# Rust: Multithreading
+
+https://priver.dev/blog/rust/multi-threading/
---
+++ learning-playgrounds/This-Week-In-Rust/multi-threading/src/main.rs
@@ -0,0 +1,142 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+    time::Duration,
+};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    example_sync();
+
+    let one = tokio::spawn(async {
+        println!("Hello from one!");
+    });
+
+    let two = tokio::spawn(async {
+        println!("Hello from two!");
+    });
+
+    one.await.unwrap();
+    two.await.unwrap();
+
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+
+    let mut a = vec![1, 2, 3];
+    let mut x = 0;
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!("hello from the first scoped thread");
+            // We can borrow `a` here.
+            dbg!(&a);
+        });
+        s.spawn(|| {
+            println!("hello from the second scoped thread");
+            // We can even mutably borrow `x` here,
+            // because no other threads are using it.
+            x += a[0] + a[2];
+        });
+        println!("hello from the main thread");
+    });
+
+    // After the scope, we can modify and access our variables again:
+    a.push(4);
+    assert_eq!(x, a.len());
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn example_sync() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", &v);
+    });
+
+    handle.join().unwrap();
+
+    let thread_one = thread::spawn(|| {
+        println!("Hello from thread one!");
+    });
+
+    let thread_two = thread::spawn(|| {
+        println!("Hello from thread two!");
+    });
+
+    thread_one.join().unwrap();
+    thread_two.join().unwrap();
+}
